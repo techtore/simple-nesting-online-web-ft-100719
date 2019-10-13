a4ce4cf8a29d190cfad4a6fc03801e8e66e6a90a
Victoria Hale <vic.hale74@gmail.com>
2019-10-13 00:05:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,13 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto][:known_for][:languages]
+    programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
+    
+  programmer_hash
  
 end
 
